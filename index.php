3ac78c1aeb585cfc69a9b4d8affb206b3b0f0bde
momouzunov2003 <uzunovvv03@gmail.com>
2025-05-20 17:35:05 +0300

    Create index.php

---
+++ index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE  html>
+<html lang="en">    
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Pic2Map Fake</title>
+    </head>
+<body>
+
+    <?php
+    echo "kole poluchi li?";
+    echo "<br>";
+    ?>
+</body>    
+</html>
