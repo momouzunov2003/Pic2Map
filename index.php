00a5b8a0ab28e6a774d3b075b5f3fe3b2b4f2098
Georgi Iliev <georgi.iliev533@outlook.com>
2025-05-20 18:30:21 +0300

    Created main page

--- index.php
+++ index.php
@@ -4,12 +4,58 @@
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <title>Pic2Map Fake</title>
+        <title>Pic2Map</title>
+        <link rel="stylesheet" href="./css/main.css">
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.6/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-4Q6Gf2aSP4eDXB8Miphtr37CMZZQ5oXLH2yaXMJ2w8e2ZtHTl7GptT4jmndRuHDT" crossorigin="anonymous">
+        <link rel="stylesheet" href="https://unpkg.com/leaflet@1.9.4/dist/leaflet.css" />
+
     </head>
 <body>
+    <div id="map"></div>
 
-    <?php
-    echo "kole poluchi li?";
-    echo "<br>";
-    ?>
-</body>    
+    <div class="overlay d-flex flex-column">
+            <header class="bg-light p-3 d-flex justify-content-between align-items-center shadow-sm">
+                <div class="fw-bold fs-4">Logo</div>
+                    <nav>
+                        <a href="#" class="btn btn-outline-primary me-2">Home</a>
+                        <a href="#" class="btn btn-outline-secondary">Contact</a>
+                    </nav>
+            </header>
+
+        <main class="main-content">
+            <button class="btn btn-primary btn-lg shadow">Create a gallery</button>
+        </main>
+
+        <footer class="bg-light text-center py-3 shadow-sm">
+            &copy; <script>document.write(new Date().getFullYear())</script> Pic2Map Fake. <a href="#">Terms and Conditions</a>
+        </footer>
+    </div>
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
+
+    <script src="https://unpkg.com/leaflet/dist/leaflet.js"></script>
+    <script>
+        const map = L.map('map').setView([0, 0], 2);
+
+        L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+            attribution: '&copy; OpenStreetMap contributors'
+        }).addTo(map);
+
+        if (navigator.geolocation) {
+            navigator.geolocation.getCurrentPosition(
+                (position) => {
+                    const lat = position.coords.latitude;
+                    const lng = position.coords.longitude;
+                    map.setView([lat, lng], 16);
+                },
+                (err) => {
+                    console.warn("Geolocation failed or was denied.");
+                    map.setView(["42.674349403151", "23.330461580825677"], 16);
+                }
+            );
+        } else {
+            console.warn("Geolocation not supported by this browser.");
+        }
+    </script>
+</body>
 </html>
