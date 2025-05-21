b5296f4f685170e0cdc9348173a1603560b6fb5c
Georgi Iliev <georgi.iliev533@outlook.com>
2025-05-21 13:34:23 +0300

    Fixed links again

--- index.php
+++ index.php
@@ -1,17 +1,18 @@
 <!DOCTYPE  html>
+<?php define('APP_ROOT', '/pic2map'); ?>
 <html lang="en">    
     <head>
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <title>Pic2Map</title>
-        <link rel="stylesheet" href="./css/main.css">
+        <link rel="stylesheet" href="<?= APP_ROOT ?>/css/main.css">
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.6/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-4Q6Gf2aSP4eDXB8Miphtr37CMZZQ5oXLH2yaXMJ2w8e2ZtHTl7GptT4jmndRuHDT" crossorigin="anonymous">
         <link rel="stylesheet" href="https://unpkg.com/leaflet@1.9.4/dist/leaflet.css" />
 
-        <link rel="apple-touch-icon" sizes="180x180" href="./img/favicon/apple-touch-icon.png">
-        <link rel="icon" type="image/png" sizes="32x32" href="./img/favicon/favicon-32x32.png">
-        <link rel="icon" type="image/png" sizes="16x16" href="./img/favicon/favicon-16x16.png">
-        <link rel="manifest" href="./img/favicon/site.webmanifest">
+        <link rel="apple-touch-icon" sizes="180x180" href="<?= APP_ROOT ?>/img/favicon/apple-touch-icon.png">
+        <link rel="icon" type="image/png" sizes="32x32" href="<?= APP_ROOT ?>/img/favicon/favicon-32x32.png">
+        <link rel="icon" type="image/png" sizes="16x16" href="<?= APP_ROOT ?>/img/favicon/favicon-16x16.png">
+        <link rel="manifest" href="<?= APP_ROOT ?>/img/favicon/site.webmanifest">
     </head>
 <body>
     <div id="map"></div>
@@ -19,12 +20,14 @@
     <div class="overlay d-flex flex-column">
             <header class="bg-light p-3 d-flex justify-content-between align-items-center shadow-sm">
                 <div class="logo-container d-flex align-items-center">
-                    <a href="./"><img src="./img/logo.png" alt="Logo" class="logo" width="70" height="70"></a>
+                    <a href="<?= APP_ROOT ?>/">
+						<img src="<?= APP_ROOT ?>/img/logo.png" alt="Logo" class="logo" width="70" height="70">
+					</a>
                 </div>
                 <nav>
-                    <a href="./random" class="btn btn-outline-danger me-2">Random</a>
-                    <a href="./" class="btn btn-outline-primary me-2">Home</a>
-                    <a href="./contacts" class="btn btn-outline-secondary">Contact</a>
+                    <a href="<?= APP_ROOT ?>/random" class="btn btn-outline-danger me-2">Random</a>
+                    <a href="<?= APP_ROOT ?>/" class="btn btn-primary disabled me-2">Home</a>
+                    <a href="<?= APP_ROOT ?>/contacts" class="btn btn-outline-secondary">Contact</a>
                 </nav>
             </header>
 
@@ -33,7 +36,7 @@
         </main>
 
         <footer class="bg-light text-center py-3 shadow-sm">
-            &copy; <script>document.write(new Date().getFullYear())</script> Pic2Map Fake. <a href="./tc">Terms and Conditions</a>
+            &copy; <script>document.write(new Date().getFullYear())</script> Pic2Map Fake. <a href="<?= APP_ROOT ?>/tc">Terms and Conditions</a>
         </footer>
     </div>
 
